e3c21f3e12ba6aed88474dec8374b2f7e7aa4a4b
Mangl_E <austinlee818@naver.com>
2022-11-07 17:44:37 +0900

    asdf

---
+++ code/settings.py
@@ -0,0 +1,15 @@
+level_map = [
+'                '
+'XXX             '
+'                '
+'XXX             '
+'                '
+'                '
+'P               '
+'XXX          XXX'
+'XXXXXXXXXXXXXXXX'
+]
+
+screen_width = 1920
+screen_height = 1080
+tile_size = 120
